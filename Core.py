e85de15bc221fe001aa02969f53a1cd54251e10d
smatsumori <shoya.matsumori@gmail.com>
2019-12-01 14:57:57 +0900

    MOD: refactor. abide pep8

--- Core.py
+++ Core.py
@@ -1,4 +1,4 @@
-#Subprocessの実行ファイル
+# Subprocessの実行ファイル
 
 import subprocess
 import time
@@ -12,13 +12,12 @@ import struct
 # from Julius import voice_thread
 # from Sensor import dist_thread
 
-#コマンドライン入力で引数でtestと入力すると，testモードになってstubが自動実行される
-
+# コマンドライン入力で引数でtestと入力すると，testモードになってstubが自動実行される
 argv = sys.argv
 if len(argv) > 1:
-	is_test = argv[1] == 'test'
+    is_test = argv[1] == 'test'
 else:
-	is_test = False
+    is_test = False
 
 proc = {}
 
@@ -30,34 +29,35 @@ proc = {}
 # print (json.dumps(request))
 
 
-#アプリの実行と終了
+# アプリの実行と終了
 nullFile = open('/dev/null', 'w')
 
 proc['app'] = subprocess.Popen(
-	['python3', '-u', './app/dist_motor_app.py'],
-	stdin = subprocess.PIPE,
-	stdout=subprocess.PIPE
+    ['python3', '-u', './app/dist_motor_app.py'],
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE
 )
 
-def exitCore():#voice_threadで使用
-	app = proc.pop('app')
-	for process in proc.values():
-		process.terminate()
-	app.terminate()
-	sys.exit()
+
+def exitCore():  # voice_threadで使用
+    app = proc.pop('app')
+    for process in proc.values():
+        process.terminate()
+    app.terminate()
+    sys.exit()
 
 # if not is_test:
-	# proc['julius'] = subprocess.Popen(
-	# 	#julius -C ~/work/julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/word.jconf -module > /dev/null &
-	# 	['~/work/julius/julius-4.4.2/julius/julius', '-C', '~/work/julius-4.4.2/julius-kit/dictation-kit-v4.4.2-linux/word.jconf', '-module'],
-	# 	stdout=nullFile
-	# )
+    # proc['julius'] = subprocess.Popen(
+    # 	#julius -C ~/work/julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/word.jconf -module > /dev/null &
+    # 	['~/work/julius/julius-4.4.2/julius/julius', '-C', '~/work/julius-4.4.2/julius-kit/dictation-kit-v4.4.2-linux/word.jconf', '-module'],
+    # 	stdout=nullFile
+    # )
 
-	# proc['voice'] = subprocess.Popen(
-	# 	["./Julius/julius_start.sh"],
-	# 	stdout=subprocess.PIPE,
-	# 	shell=True
-	# )
+    # proc['voice'] = subprocess.Popen(
+    # 	["./Julius/julius_start.sh"],
+    # 	stdout=subprocess.PIPE,
+    # 	shell=True
+    # )
 
 # #Camera
 # if is_test:#単純にカメラを立ち上げる
@@ -70,18 +70,19 @@ def exitCore():#voice_threadで使用
 # 	stdout = subprocess.PIPE
 # )
 
-#Main Motor
-if (is_test):
-	#自動で前後左右動く
-	motor_cmd = ['python3', '-u', './Motor/motor_stub.py']
+
+# Main Motor
+if is_test:
+    # 自動で前後左右動く
+    motor_cmd = ['python3', '-u', './Motor/motor_stub.py']
 # else:
 # 	#音声入力に応じて実行させたりする
 # 	motor_cmd = ['python3', '-u', './Motor/motor_thread.py']
-proc['motor'] = subprocess.Popen(
-	motor_cmd,
-	stdin = subprocess.PIPE,
-	stdout=subprocess.PIPE
-)
+    proc['motor'] = subprocess.Popen(
+        motor_cmd,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
 
 # #Servo Motor
 # if is_test:#自動でくるくる動く
@@ -94,29 +95,28 @@ proc['motor'] = subprocess.Popen(
 # 	stdout = subprocess.PIPE
 # )
 
-#Distance Sensor
+# Distance Sensor
 if is_test:
-	#自動で距離を測って出力
-	sensor_cmd = ['python3', '-u', './Sensor/dist_stub.py']
+    # 自動で距離を測って出力
+    sensor_cmd = ['python3', '-u', './Sensor/dist_stub.py']
 # else:
 # 	#motorとかと連動
 # 	motor_cmd = ['python3', '-u', './Sensor/dist_thread.py']
-proc['dist'] = subprocess.Popen(
-	sensor_cmd,
-	stdin = subprocess.PIPE,
-	stdout=subprocess.PIPE
-)
+    proc['dist'] = subprocess.Popen(
+        sensor_cmd,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
 
 
-
-#他のファイルからの呼び出し?
+# 他のファイルからの呼び出し?
 threads = {}
 
 # 複数のスレッドを実行する時には，
 # threads['']=...
 # threads[''].start()
 
-#とりあえず
+# とりあえず
 
 # def func_voice():
 # 	threads['voice'] = voice_thread.VoiceThread(
@@ -158,39 +158,41 @@ threads = {}
 # 	)
 # 	threads['sensor'].start()
 
-#voice threadの呼び出し
-#fnuc_voice()
+# voice threadの呼び出し
+# fnuc_voice()
 
-# while True:
-	#アプリケーション作成時に，proc[app]に書き込まれたものを読み込んで実行
-	# raw_request = proc['app'].stdout.readline()
-	# try:
-	# 	request = json.loads(raw_request)
-	# except ValueError:
-		# continue
-
-	#センサーから毎秒距離を取得する
-	#音声からは毎秒コマンドを
-	# →Motorに送る
-
-	# #センサーからは数字のみを渡す
-	# sensor_rep = json.loads(proc['sensor'].stdout.readline())
-	# request['dist'] = sensor_rep['dist']
-	# #voiceからはいろんなコマンドが送られる
-	# # →requestと同じ辞書形式で(コピーを渡してそこに書き込み,)jsonに書き込み
-	# voice_rep = json.loads(proc['voice'].stdout.readline())
-	# request['motor_cmd'] = voice_rep['motor_cmd']
-	# #motorに送る
-	# proc['motor'].stdout.write(json.dumps(request))
-
-	# if request['module'] == 'camera':
-	# 	func_camera(request)
-	# elif request['module'] == 'voice':
-	# 	func_voice(request)
-	# elif request['module'] == 'motor':
-	# 	func_motor(request)
-	# elif request['module'] == 'sensor':
-	# 	func_sensor(request)
-	# elif request['module'] == 'quit':
-	# 	time.sleep(3)
-	# 	break
+# アプリケーション作成時に，proc[app]に書き込まれたものを読み込んで実行
+'''
+while True:
+    raw_request = proc['app'].stdout.readline()
+    try:
+    	request = json.loads(raw_request)
+    except ValueError:
+    continue
+    
+    センサーから毎秒距離を取得する
+    音声からは毎秒コマンドを
+    →Motorに送る
+    
+    #センサーからは数字のみを渡す
+    sensor_rep = json.loads(proc['sensor'].stdout.readline())
+    request['dist'] = sensor_rep['dist']
+    #voiceからはいろんなコマンドが送られる
+    # →requestと同じ辞書形式で(コピーを渡してそこに書き込み,)jsonに書き込み
+    voice_rep = json.loads(proc['voice'].stdout.readline())
+    request['motor_cmd'] = voice_rep['motor_cmd']
+    #motorに送る
+    proc['motor'].stdout.write(json.dumps(request))
+    
+    if request['module'] == 'camera':
+    	func_camera(request)
+    elif request['module'] == 'voice':
+    	func_voice(request)
+    elif request['module'] == 'motor':
+    	func_motor(request)
+    elif request['module'] == 'sensor':
+    	func_sensor(request)
+    elif request['module'] == 'quit':
+	time.sleep(3)
+	break
+'''
