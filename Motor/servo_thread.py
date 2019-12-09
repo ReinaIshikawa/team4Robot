3034e6292f2a83caccc59a260f1651e7bf72a82a
ywatanabe10 <yuta.watanabe@keio.jp>
2019-12-09 16:47:19 +0900

    change servo files

--- Motor/servo_thread.py
+++ Motor/servo_thread.py
@@ -7,11 +7,35 @@ import json
 import servo
 
 class ServoThread(threading.Thread):
-	def _init_(self, request, servo):
+	def __init__(self, request, servo, app):
+		super(ServoThread, self).__init__()
 		self.request = request
 		self.servo = servo
+		self.app = app
+		
 
 	def run(self):
 		servo0 = servo.servo_Class(Channel=0, ZeroOffset=-5)
-		# servo1 = servo.servo_Class(Channel=1, ZeroOffset=-5)
+		servo1 = servo.servo_Class(Channel=1, ZeroOffset=-5)
 		# servo2 = servo.servo_Class(Channel=2, ZeroOffset=-5)
+
+		while True:
+			#縦に動かす
+			if self.request['cmd'] == 'clean':
+				servo0.rotate(45, 90)
+				time.sleep(1)
+
+			#横に動かす
+			if self.request['cmd'] == 'attack':
+				servo1.rotate(45, 90)
+				time.sleep(1)
+				
+			#止める
+			if self.request['cmd'] == 'quit':
+				servo0.rotate(-1, -1)
+				servo1.rotate(-1, -1)
+				time.sleep(1)
+				
+
+
+
