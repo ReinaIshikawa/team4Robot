a9dce466e5c30b1ddfc5bc5889c34c4a40954e47
ywatanabe10 <yuta.watanabe@keio.jp>
2019-12-18 15:26:38 +0900

    change

---
+++ Julius/voice_stub.py
@@ -0,0 +1,19 @@
+import time
+import threading
+
+
+class VoiceStub(threading.Thread):
+    def __init__(self, app):
+        super(VoiceStub, self).__init__()
+        self.app = app
+        self.num = 0
+        self.lst = ['カメラ', '前', '後ろ', '進め', '右', '左', 'とまれ', 'ついて来て', '終了']
+    
+    def run(self, request=None):
+        if not request:
+            return
+        print('voice_thread->voice: {}:{}'.format(self.lst[num], request))
+        self.num += 1
+        if self.num == 9:
+            self.num = 0
+            
