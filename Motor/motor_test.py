25cbbfab2d26e8c07a30d8b2828663effc7d6c58
daruma3 <motonari.k714@gmail.com>
2020-01-20 15:45:18 +0900

    ADD motor_test.py

---
+++ Motor/motor_test.py
@@ -0,0 +1,25 @@
+import wiringpi as wp
+import time
+import struct
+import Motor
+import Motor_move as Mmove
+
+if __name__=="__main__":
+    speed = 2000
+
+    print("***** start spi test program *****")
+
+    right= Mmove.Motor_move(0,speed)
+    left= Mmove.Motor_move(1,speed)
+
+    while True:
+        #speed = speed + 2000 # 30000 位まで
+        right.Run_forward()
+        left.Run_forward()
+        time.sleep(5)
+        right.Softstop()
+        left.Softstop()
+        right.Softhiz()
+        left.Softthiz()
+        quit()
+    quit()
